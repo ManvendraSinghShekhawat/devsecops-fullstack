6391395f58243edd9d8479276aaea89ac45eafca
Manvendra Singh Shekhawat <65719012+ManvendraSinghShekhawat@users.noreply.github.com>
2025-10-22 23:08:14 +0100

    Update aws_infra_spec.rb

--- inspec/controls/aws_infra_spec.rb
+++ inspec/controls/aws_infra_spec.rb
@@ -1,1 +1,10 @@
+control 's3-backup-bucket' do
+  impact 1.0
+  title 'S3 backup bucket has versioning and encryption'
+  describe aws_s3_bucket(bucket_name: attribute('s3_bucket', default: '<S3_BUCKET_NAME>')) do
+    it { should exist }
+    it { should have_versioning_enabled }
+    it { should have_default_encryption_enabled }
+  end
+end
 
