bf9f3c8823671529d1b387fa24d72805db384c6a
Manvendra Singh Shekhawat <65719012+ManvendraSinghShekhawat@users.noreply.github.com>
2025-10-22 22:42:35 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,5 @@
-
+variable "region" { type = string; default = "eu-west-1" }
+variable "project_name" { type = string; default = "devsecops-fullstack" }
+variable "ssh_public_key_path" { type = string; default = "~/.ssh/id_rsa.pub" }
+variable "instance_type" { type = string; default = "t3.micro" }
+variable "my_ip_cidr" { type = string; default = "9.79.201.188/32" }
