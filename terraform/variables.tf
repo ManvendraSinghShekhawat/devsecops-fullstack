bf31a8c5880b93aa71bf68e3ed1617b581108316
Manvendra Singh Shekhawat <65719012+ManvendraSinghShekhawat@users.noreply.github.com>
2025-10-23 10:48:00 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,35 +1,23 @@
-variable "region" {
+variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
   default     = "eu-west-1"
 }
 
 variable "project_name" {
-  description = "Project name used in tags"
+  description = "Prefix used for resource naming"
   type        = string
   default     = "devsecops-fullstack"
 }
 
-variable "ssh_public_key_path" {
-  description = "Path to your SSH public key file (to register as AWS key pair)"
-  type        = string
-  default     = "~/.ssh/id_rsa.pub"
-}
-
-variable "instance_type" {
-  description = "EC2 instance type (free-tier: t3.micro/t2.micro)"
-  type        = string
-  default     = "t3.micro"
-}
-
 variable "my_ip_cidr" {
-  description = "Your public IP in CIDR notation (e.g., 1.2.3.4/32). Replace before apply."
+  description = "Your public IP in CIDR format for SSH access"
   type        = string
   default     = "9.79.201.188/32"
 }
 
-variable "s3_backup_bucket" {
-  description = "S3 bucket name for backups (must be globally unique). Replace with your bucket name."
+variable "node_instance_type" {
+  description = "EC2 instance type for EKS nodes"
   type        = string
-  default     = "<S3_BUCKET_NAME>"
+  default     = "t3.micro"
 }
