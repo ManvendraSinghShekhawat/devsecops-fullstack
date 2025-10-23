e105159bc7c2e186e18f2281aadf4c16614c6b3b
Manvendra Singh Shekhawat <65719012+ManvendraSinghShekhawat@users.noreply.github.com>
2025-10-23 10:52:39 +0100

    Update s3-backups.tf

--- terraform/s3-backups.tf
+++ terraform/s3-backups.tf
@@ -1,1 +1,21 @@
+resource "aws_s3_bucket" "backups" {
+  bucket = "${var.project_name}-backups-${random_id.bucket_id.hex}"
+  force_destroy = true
+  versioning {
+    enabled = true
+  }
+  lifecycle_rule {
+    enabled = true
+    expiration {
+      days = 30
+    }
+  }
+  tags = {
+    Name = "${var.project_name}-backups"
+  }
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
 
