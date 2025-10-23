3d7942b8bca7990ff68fa1207c8df7b4107d77bf
Manvendra Singh Shekhawat <65719012+ManvendraSinghShekhawat@users.noreply.github.com>
2025-10-23 10:52:21 +0100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,12 @@
+output "ecr_repo_url" {
+  value = aws_ecr_repository.app_repo.repository_url
+}
+
+output "eks_cluster_name" {
+  value = aws_eks_cluster.main.name
+}
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.main.name
+}
 
